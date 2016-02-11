fce3ff07e02601457c6459f0e325cd907ae29a3a
duanepitre <duane.pitre@gmail.com>
2016-02-11 12:38:18 -0600

    Update PHPform-2_PHP.php

--- PHPform-2_PHP.php
+++ PHPform-2_PHP.php
@@ -1,36 +1,36 @@
 <?php
-  $email_to = "dpitre1@tulane.edu"; 
-  $email_subject = "Media Services Request Form";
-  $email_from = $_POST['email'];
+	$email_to = "dpitre1@tulane.edu"; 
+	$email_subject = "Media Services Request Form";
+	$email_from = $_POST['email'];
 	
-  function died($error) {
-    // error code
-    echo "We are very sorry, but there were error(s) found with the form you submitted. ";
-    echo "These errors appear below.<br /><br />";
-    echo $error."<br /><br />";
-    echo "Please go back and fix these errors.<br /><br />";
-    die();
-  }
+	function died($error) {
+	// error code
+	echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+	echo "These errors appear below.<br /><br />";
+	echo $error."<br /><br />";
+	echo "Please go back and fix these errors.<br /><br />";
+	die();
+	}
   
-  $error_message = "";
-  $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+	$error_message = "";
+	$email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
 	if(!preg_match($email_exp,$email_from)) {
 		$error_message .= 'The Email Address you entered does not appear to be valid.<br />';
 	}
   
-  $string_exp = "/^[A-Za-z .'-]+$/";
+  	$string_exp = "/^[A-Za-z .'-]+$/";
  
 	if(strlen($error_message) > 0) {
 	  died($error_message);
 	}
-// Email test formatting
-  $email_message = "Form details below...\n\n";
+	// Email test formatting
+	 $email_message = "Form details below...\n\n";
  
-  function clean_string($string) {
-      $bad = array("content-type","bcc:","to:","cc:","href");
-      return str_replace($bad,"",$string);
-  }
+	function clean_string($string) {
+		$bad = array("content-type","bcc:","to:","cc:","href");
+		return str_replace($bad,"",$string);
+	}
  
         if(!empty($_POST['fullname'])) {
         	$email_message .= "Name: ".clean_string($_POST['fullname'])."\n";
